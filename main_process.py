021c96d83015e6818f3d9cc5a0f9c01269ef26f5
fanyucai1 <fanyucai1@126.com>
2019-12-17 14:44:36 +0800

    no

--- main_process.py
+++ main_process.py
@@ -178,10 +178,8 @@ def run(outdir,SampleSheet,rundir,configfile,target,probe,name,method,vaf,pon,cn
         for file in files:
             tmp=os.path.join(root,file)
             sample=tmp.split("/")[-2]
-            if tmp.endswith("annovar.filter.tsv") or tmp.endswith("annovar.tsv"):
+            if tmp.endswith("annovar.filter.tsv") or tmp.endswith("annovar.tsv") or tmp.endswith("MetricsReport.tsv"):
                     core.copy_result.run(tmp, "/media/CAP/fyc_test/%s/" % (sample))
-            if tmp.endswith("MetricsReport.tsv"):
-                core.copy_result.run(tmp, "/media/CAP/fyc_test/%s/" % (sample))
             if tmp.endswith("%s.tsv"%(sample)) and tmp.split("/")[-2]=="fusion":
                 core.copy_result.run(tmp, "/media/CAP/fyc_test/%s/" % (sample))
     end=time.strftime("%Y%m%d_%H:%M:%S", time.localtime())
